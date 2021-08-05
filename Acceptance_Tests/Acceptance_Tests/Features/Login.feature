116faaf04e322c72a8f6c4fd7cd5aee834d4b38c
Junjian-Huang <junjian.huang@hotmail.com>
2021-08-06 11:40:10 +1200

    updated testing code and the running result screenshot

---
+++ Acceptance_Tests/Acceptance_Tests/Features/Login.feature
@@ -0,0 +1,10 @@
+﻿Feature: Login
+
+@smoke
+Scenario: Login to app
+	Given I launch the website
+	And I enter the my username and password
+		| UserName | Password |
+		| hjjgames | j1234567 |
+	When I click on the login button
+	Then the I should see my username
---
+++ Acceptance_Tests/Acceptance_Tests/Steps/LoginSteps.cs
@@ -0,0 +1,43 @@
+﻿using Acceptance_Tests.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace Acceptance_Tests.Steps
+{
+    [Binding]
+    public class LoginSteps
+    {
+        FrontPage frontPage = null;
+
+        [Given(@"I launch the website")]
+        public void GivenILaunchTheWebsite()
+        {
+            IWebDriver webDriver = new ChromeDriver();
+            webDriver.Navigate().GoToUrl("https://en.reddit.com/");
+            frontPage = new FrontPage(webDriver);
+        }
+
+        [Given(@"I enter the my username and password")]
+        public void GivenIEnterTheMyUsernameAndPassword(Table table)
+        {
+            dynamic data = table.CreateDynamicInstance();
+            frontPage.Login((string)data.UserName, (string)data.Password);
+        }
+
+        [When(@"I click on the login button")]
+        public void GivenIClickOnTheLoginButton()
+        {
+            frontPage.ClickLoginButton();
+        }
+
+        [Then(@"the I should see my username")]
+        public void ThenTheIShouldSeeMyUsername()
+        {
+            Assert.That(frontPage.IsXpathExists, Is.True);
+        }
+    }
+}
